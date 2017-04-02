91e3f961985c07b451063e241a2026a1e7733519
Nobita_dgut <13790455206@163.com>
2017-04-02 10:33:57 +0800

    Revert "Revert "Revert "mysql"""
    This reverts commit d9e6878ab2f26f65016ce7a2d7253c6b238b5101.

--- src/main/java/com/gd/test/component/demo/service/IBananaService.java
+++ src/main/java/com/gd/test/component/demo/service/IBananaService.java
@@ -1,13 +1,9 @@
 package com.gd.test.component.demo.service;
 
-import java.util.List;
-
 import com.gd.test.component.demo.bean.User;
 
 public interface IBananaService {
 	String getJson();
 
 	User addUser();
-
-	List<User> getAllUser() throws Exception;
 }
--- src/main/java/com/gd/test/component/demo/service/impl/BananaServiceImpl.java
+++ src/main/java/com/gd/test/component/demo/service/impl/BananaServiceImpl.java
@@ -1,64 +1,28 @@
 package com.gd.test.component.demo.service.impl;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.gd.test.component.demo.bean.User;
 import com.gd.test.component.demo.dao.IBananaDao;
-import com.gd.test.component.demo.dao.IUserMapper;
 import com.gd.test.component.demo.service.IBananaService;
-
-import utils.CommonUtils;
-import utils.FileUtils;
 
 @Service("bananaService")
 public class BananaServiceImpl implements IBananaService {
 
 	@Resource
 	private IBananaDao bananaDao;
-	@Resource
-	private IUserMapper userMapper;
 
 	@Override
 	public String getJson() {
 		System.out.println(bananaDao.toString());
-		return bananaDao.getJson();
+		return bananaDao.getJson()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
 	}
 
 	@Override
 	public User addUser() {
-
-		User user = new User();
-
-		String newPort = FileUtils.getTheNextPort();
-		String pwd = CommonUtils.getPassWordOne(10);
-
-		if (newPort == null || pwd == null)
-			return null;
-
-		FileUtils.addUser(newPort, pwd);
-		FileUtils.recordPort(newPort);
-		userMapper.addUser(newPort, pwd);
-		// 重启shadowsock服务
-
-		CommonUtils.getInstance().restartShadowsock();
-
-		user.user_password = pwd;
-		user.user_port = newPort;
-
-		return user;
-	}
-
-	@Override
-	public List<User> getAllUser() throws Exception {
-		List<User> userlist = userMapper.getAllUsers();
-		for (int i = 0; i < userlist.size(); i++) {
-			System.out.println(userlist.get(i).user_port);
-		}
-		return userlist;
+		return bananaDao.addUser();
 	}
 
 }
